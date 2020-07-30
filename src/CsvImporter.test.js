ddefc7c8f2269ed00d2c9bd54c54fd7c37f65b6c
Samuel Reid <sam.reid@qsrsoft.com>
2020-07-30 01:20:12 -0500

    Basic unit test for CsvImporter component

---
+++ src/CsvImporter.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CsvImporter from './CsvImporter';
+
+describe('<CsvImporter />', () => {
+    test('clicking the button triggers a click on the input element', () => {
+        const wrapper = mount(<CsvImporter model="people" />);
+        const mockCallback = jest.fn();
+        wrapper.find('input').getDOMNode().onclick = mockCallback;
+        wrapper.find('button').simulate('click');
+        expect(mockCallback.mock.calls.length).toEqual(1);
+    });
+});
